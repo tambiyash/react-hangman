6e9fa474b750d3215b9d4bccea392bc3939ba97f
Yash Tambi <ytambi@pypestream.com>
2020-07-15 14:16:16 +0530

    Updates

---
+++ precache-manifest.64fa07ff7791106751d2df82285e57a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5148d036383f3d0bd962533f19eed70",
+    "url": "/react-hangman/index.html"
+  },
+  {
+    "revision": "f18d8581fdc832a320b0",
+    "url": "/react-hangman/static/css/main.35b0b5df.chunk.css"
+  },
+  {
+    "revision": "afe61f3d137f266a72df",
+    "url": "/react-hangman/static/js/2.0fab1ee7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hangman/static/js/2.0fab1ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18d8581fdc832a320b0",
+    "url": "/react-hangman/static/js/main.fbadb868.chunk.js"
+  },
+  {
+    "revision": "e4bdc9962578a3f5d073",
+    "url": "/react-hangman/static/js/runtime-main.abe8765d.js"
+  }
+]);
