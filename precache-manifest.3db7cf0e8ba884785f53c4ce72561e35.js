55337128507c84fe82d2413e1d56791e7a11b23c
Yash Tambi <tambiyash@gmail.com>
2025-03-31 13:10:18 +0530

    Updates

---
+++ precache-manifest.3db7cf0e8ba884785f53c4ce72561e35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89dd85375018a66ade83691bb5778a9",
+    "url": "/react-hangman/index.html"
+  },
+  {
+    "revision": "419cadf040f2e821fd4b",
+    "url": "/react-hangman/static/css/main.e80920d6.chunk.css"
+  },
+  {
+    "revision": "09fba8b79093e691ab77",
+    "url": "/react-hangman/static/js/2.99290dcb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-hangman/static/js/2.99290dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419cadf040f2e821fd4b",
+    "url": "/react-hangman/static/js/main.11835f42.chunk.js"
+  },
+  {
+    "revision": "60a094aa6adb2d39e2d7",
+    "url": "/react-hangman/static/js/runtime-main.e860fd42.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-hangman/precache-manifest.64fa07ff7791106751d2df82285e57a0.js"
+  "/react-hangman/precache-manifest.3db7cf0e8ba884785f53c4ce72561e35.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-hangman/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
